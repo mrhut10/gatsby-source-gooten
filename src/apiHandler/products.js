52496faf5040e3fd74761064e48f70dd2fb04af1
mrhut10 <mrhut10@protonmail.com>
2020-02-20 01:27:39 +1100

    fixed bug in getProducts

--- src/apiHandler/products.js
+++ src/apiHandler/products.js
@@ -5,7 +5,7 @@ const { parseResponse, spy } = require('./common')
 const renameIdField = ({Products: products}) => products.map(
   product => {
     const newDefinition = {GootenId: product.Id, ...product}
-    //Object.defineProperty(product, 'GootenId', Object.getOwnPropertyDescriptor(product, 'Id'))
+    // Object.defineProperty(product, 'GootenId', Object.getOwnPropertyDescriptor(product, 'Id'))
     delete newDefinition['Id']
     return newDefinition
   }
@@ -24,27 +24,33 @@ const getProducts = ({
   const { createNode } = actions
   const {url, recipeId, countryCode, currencyCode } = options
 
-  const [allProducts, setupProducts] = [
-    await axois.get(`${url}\products`, {params: {recipeId, countryCode, currencyCode, all: true }}),
-    await axois.get(`${url}\products`, {params: {recipeId, countryCode, currencyCode }}),
-  ].map(parseResponseAndRenameIdField)
+  const apiParams = {recipeId, countryCode, currencyCode};
+  const apiResults = [
+    // products currently set up the users in Gooten's Product Hub
+    axois.get(`${url}\products`, {params: apiParams }),
+    // all products avaliable to region
+    axois.get(`${url}\products`, {params: { ...apiParams, all: true }}),
+  ].map(requestPromise => requestPromise.then(parseResponseAndRenameIdField))
+  
+  const [setupProducts, allProducts] = await Promise.all(apiResults)
 
   allProducts.forEach(product => {
     const nodeData = { ...product }
     // is the product setup by user in there productHub section of gooten website
     nodeData.HasDefinitionInProductHub = Boolean(setupProducts.find(readyProduct => readyProduct['GootenId'] === nodeData["GootenId"]))
-    const id = createNodeId(`GootenProduct-${product.gootenId}`)
+    
     const nodeMeta = {
-      id,
+      id: createNodeId(`GootenProduct-${nodeData.GootenId}`),
       parent: null,
       children: [],
       internal: {
         type: 'GootenProduct',
         mediaType: 'text/html',
-        // content: JSON.stringify(nodeData),
+        content: JSON.stringify(nodeData),
         contentDigest: createContentDigest(nodeData),
       }
     }
+    console.log(nodeData.GootenId)
     const node = Object.assign({}, nodeData, nodeMeta)
     createNode(node)
   })
