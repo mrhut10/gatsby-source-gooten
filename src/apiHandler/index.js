a66a6815d1feeb5aae6cc2f5350b67c6caf734e9
mrhut10 <mrhut10@protonmail.com>
2020-02-21 01:53:26 +1100

    added product variants to product definition

--- src/apiHandler/index.js
+++ src/apiHandler/index.js
@@ -18,21 +18,21 @@ const GetCurrencies = ({
     const request = await axois.get(`${url}/currencies`, {params: {recipeId}})
     const response = parseResponse(request)
     const { Currencies: currencies } = response
-      currencies.forEach(currency => {
+    currencies.forEach(currency => {
       ''.toLowerCase
       const {Name, Code, Format} = currency
       const nodeData = {Name, Code, Format, Selected: currencyCode && currencyCode.toLowerCase() === Code.toLowerCase()}
-        const nodeMeta = {
+      const nodeMeta = {
         id: createNodeId(`Gootencurrency-${Code}`),
-          parent: null,
-          children: [],
-          internal: {
-            type: 'GootenCurrency',
-            mediaType: 'text/html',
-            content : JSON.stringify(nodeData),
-            contentDigest: createContentDigest(nodeData),
-          },
-        }
+        parent: null,
+        children: [],
+        internal: {
+          type: 'GootenCurrency',
+          mediaType: 'text/html',
+          content : JSON.stringify(nodeData),
+          contentDigest: createContentDigest(nodeData),
+        },
+      }
       createNode({...nodeData, ...nodeMeta})
     })
     if (!currencies.find(currency => currency.Code.toLowerCase() === currencyCode.toLowerCase())) console.warn(`currencyCode: ${currencyCode} doesn't appear to be a valid option please verify`)
--- src/apiHandler/products.js
+++ src/apiHandler/products.js
@@ -13,6 +13,31 @@ const renameIdField = ({Products: products}) => products.map(
 
 const parseResponseAndRenameIdField = input => renameIdField( parseResponse( input ) )
 
+const _getProductVariants = options => async productId => {
+  const {url, recipeId, countryCode, currencyCode } = options
+  const apiParams = {recipeId, countryCode, currencyCode, productId}
+
+  if (url, recipeId, countryCode, currencyCode, productId){
+    const apiResults = [
+      axois.get(`${url}\productvariants`, {params: apiParams}),
+      axois.get(`${url}\productvariants`, {params: { ...apiParams, all:true }}),
+    ].map(request => request.then(parseResponse))
+
+    const [{ProductVariants: setupVariants}, {ProductVariants: allVariants}] = await Promise.all(apiResults)
+
+    return allVariants
+      ? allVariants.map(
+        variant => ({
+          ...variant,
+          // HasDefinitionInProductHub:
+        })
+      )
+      : []
+  } else {
+    return []
+  }
+}
+
 
 const getProducts = ({
   actions,
@@ -23,37 +48,39 @@ const getProducts = ({
 }) => async options => {
   const { createNode } = actions
   const {url, recipeId, countryCode, currencyCode } = options
+  const getProductVariants = _getProductVariants(options)
 
   const apiParams = {recipeId, countryCode, currencyCode};
-  const apiResults = [
-    // products currently set up the users in Gooten's Product Hub
-    axois.get(`${url}\products`, {params: apiParams }),
-    // all products avaliable to region
-    axois.get(`${url}\products`, {params: { ...apiParams, all: true }}),
-  ].map(requestPromise => requestPromise.then(parseResponseAndRenameIdField))
-  
-  const [setupProducts, allProducts] = await Promise.all(apiResults)
 
-  allProducts.forEach(product => {
-    const nodeData = { ...product }
-    // is the product setup by user in there productHub section of gooten website
-    nodeData.HasDefinitionInProductHub = Boolean(setupProducts.find(readyProduct => readyProduct['GootenId'] === nodeData["GootenId"]))
-    
-    const nodeMeta = {
-      id: createNodeId(`GootenProduct-${nodeData.GootenId}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: 'GootenProduct',
-        mediaType: 'text/html',
-        content: JSON.stringify(nodeData),
-        contentDigest: createContentDigest(nodeData),
+  if (url, recipeId && countryCode && currencyCode){
+    const apiResults = [
+      // products currently set up the users in Gooten's Product Hub
+      axois.get(`${url}\products`, { params: apiParams }),
+      // all products avaliable to region
+      axois.get(`${url}\products`, { params: { ...apiParams, all: true } } )
+    ].map(request => request.then(parseResponseAndRenameIdField))
+
+    const [setupProducts, allProducts] = await Promise.all(apiResults)
+    allProducts.forEach(async product => {
+      const nodeData = { ...product }
+      // is the product setup by user in there productHub section of gooten website
+      nodeData.HasDefinitionInProductHub = Boolean(setupProducts.find(readyProduct => readyProduct['GootenId'] === nodeData["GootenId"]))
+      nodeData.ProductVariants = Boolean(nodeData.HasAvailableProductVariants) ? await getProductVariants(nodeData.GootenId) : []
+      const nodeMeta = {
+        id: createNodeId(`GootenProduct-${nodeData.GootenId}`),
+        parent: null,
+        children: [], // await getProductVariant(nodeData.GootenId),
+        internal: {
+          type: 'GootenProduct',
+          mediaType: 'text/html',
+          content: JSON.stringify(nodeData),
+          contentDigest: createContentDigest(nodeData),
+        }
       }
-    }
-    console.log(nodeData.GootenId)
-    const node = Object.assign({}, nodeData, nodeMeta)
-    createNode(node)
-  })
+      //const node = Object.assign({}, nodeData, nodeMeta)
+      createNode({...nodeData, ...nodeMeta})
+    })
+  }
 }
 
 exports.default = getProducts
