60b30834d97184308756b03c27ddccde10dc532e
mrhut10 <mrhut10@protonmail.com>
2020-03-03 03:02:52 +1100

    moved currencies file

--- src/apiHandler/currencies.js
+++ src/apiHandler/currencies.js
@@ -1,6 +1,41 @@
-const { parseResponse } = require('./index')
+const { parseResponse } = require('./common');
 const axois = require('axios')
 
 
+const GetCurrencies = ({
+  actions,
+  store,
+  cache,
+  createNodeId,
+  createContentDigest
+}) => async options => {
+  const { recipeId, url, currencyCode} = options
+  const { createNode } = actions
 
-// exports.default = GetCurrencies
+  if (url, recipeId){
+    const request = await axois.get(`${url}/currencies`, {params: {recipeId}})
+    const response = parseResponse(request)
+    const { Currencies: currencies } = response
+    currencies.forEach(currency => {
+      ''.toLowerCase
+      const {Name, Code, Format} = currency
+      const nodeData = {Name, Code, Format, Selected: currencyCode && currencyCode.toLowerCase() === Code.toLowerCase()}
+      const nodeMeta = {
+        id: createNodeId(`Gootencurrency-${Code}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: 'GootenCurrency',
+          mediaType: 'text/html',
+          content : JSON.stringify(nodeData),
+          contentDigest: createContentDigest(nodeData),
+        },
+      }
+      createNode({...nodeData, ...nodeMeta})
+    })
+    if (!currencies.find(currency => currency.Code.toLowerCase() === currencyCode.toLowerCase())) console.warn(`currencyCode: ${currencyCode} doesn't appear to be a valid option please verify`)
+  }
+};
+
+
+exports.default = GetCurrencies
--- src/apiHandler/index.js
+++ src/apiHandler/index.js
@@ -1,43 +1,9 @@
-const axois = require('axios')
-const { parseResponse } = require('./common')
-const GetCountryCodes = require('./countryCodes').default
-const getProducts = require('./products').default
+const axois = require('axios');
+const { parseResponse } = require('./common');
+const GetCurrencies = require('./currencies').default;
+const GetCountryCodes = require('./countryCodes').default;
+const getProducts = require('./products').default;
 
-
-const GetCurrencies = ({
-  actions,
-  store,
-  cache,
-  createNodeId,
-  createContentDigest
-}) => async options => {
-  const { recipeId, url, currencyCode} = options
-  const { createNode } = actions
-
-  if (url, recipeId){
-    const request = await axois.get(`${url}/currencies`, {params: {recipeId}})
-    const response = parseResponse(request)
-    const { Currencies: currencies } = response
-    currencies.forEach(currency => {
-      ''.toLowerCase
-      const {Name, Code, Format} = currency
-      const nodeData = {Name, Code, Format, Selected: currencyCode && currencyCode.toLowerCase() === Code.toLowerCase()}
-      const nodeMeta = {
-        id: createNodeId(`Gootencurrency-${Code}`),
-        parent: null,
-        children: [],
-        internal: {
-          type: 'GootenCurrency',
-          mediaType: 'text/html',
-          content : JSON.stringify(nodeData),
-          contentDigest: createContentDigest(nodeData),
-        },
-      }
-      createNode({...nodeData, ...nodeMeta})
-    })
-    if (!currencies.find(currency => currency.Code.toLowerCase() === currencyCode.toLowerCase())) console.warn(`currencyCode: ${currencyCode} doesn't appear to be a valid option please verify`)
-  }
-};
 
 exports.default = (
   {
@@ -51,14 +17,14 @@ exports.default = (
 ) => {
   if (options.url && options.recipeId){
     return ({
-      // list of supported currencies
+      // list of supported currencies from api
       currencies: GetCurrencies({ actions, store, cache, createNodeId, createContentDigest })(options),
-      // list of iso country codes
+      // list of iso country codes from file
       countryCodes: GetCountryCodes({ actions, store, cache, createNodeId, createContentDigest })(options),
-      // list of products and product variants
+      // list of products from api
+      // sublist of product variants from api
+      // sublist of product templates from api
       products: getProducts({ actions, store, cache, createNodeId, createContentDigest })(options),
     })
   }
 }
-
-exports.parseResponse = parseResponse
