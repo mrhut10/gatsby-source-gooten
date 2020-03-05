67d77882bc003b3459f2db327c07bd74be17184d
mrhut10 <mrhut10@protonmail.com>
2020-03-06 02:20:15 +1100

    added   retryWithGeometryInterval   adding CurrencyName to priceInfo

--- src/apiHandler/common.js
+++ src/apiHandler/common.js
@@ -15,3 +15,27 @@ export const setTimeoutPromise = (time, fn) => new Promise((resolve, reject) => 
   );
 })
 
+
+/*
+  function retryWithGeometryInterval
+  will Make a certain number of attempts to get a value from function
+  where time between each attempt increases by the geometic sequence
+    t = ratio ** i    where t is the timeInterval and i is the index of attempts
+  and where the total estimated elasp time follows the sum of geometric series
+    T = (1 - ratio ** n)/(1-ratio)
+      = ratio**n - 1
+*/
+async function retryWithGeometryInterval(fn, ratio, attempts){
+  let value;
+  Array(attempts).forEach((_, i) => {
+    value = fn();
+    if (value){
+      break;
+    } else {
+      await setTimeoutPromise(1000 * ratio ** i)
+    }
+  })
+  return value;
+}
+
+export const retryWithGeometryInterval;
